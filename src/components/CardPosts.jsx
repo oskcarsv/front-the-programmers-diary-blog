68aa7321df535fa59fcf9391a5a3b7f7089e9c4d
oskcarsv <oskcarqsv@gmail.com>
2024-05-15 19:49:19 -0600

    Refactor CardPosts component to improve code organization and styling

--- src/components/CardPosts.jsx
+++ src/components/CardPosts.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/prop-types */
 export const CardPosts = ({ title, subTitle, img }) => {
   return (
-    <div className="card-content">
+    // <div className="card-content">
+    <div className="card">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <p>{subTitle}</p>
